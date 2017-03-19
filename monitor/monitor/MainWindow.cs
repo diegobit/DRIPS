93b42dca220aed4600be14668c33f916aa44bf3e
Diego Giorgini <diego.giorgini@icloud.com>
2017-03-19 23:02:18 +0100

    Add GTK monitor project

---
+++ monitor/monitor/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gtk;
+using monitor;
+
+public partial class MainWindow : Gtk.Window
+{
+	private Monitor monitor;
+
+	public MainWindow(Monitor m) : base(Gtk.WindowType.Toplevel)
+	{
+		monitor = m;
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void onButtonClick(object sender, EventArgs e)
+	{
+		monitor.SaySomething();
+	}
+}
---
+++ monitor/monitor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+using System.IO.Ports;
+
+namespace monitor
+{
+	public class Monitor
+	{
+		public void SaySomething()
+		{
+			Console.Write("Something... ");
+		}
+
+		public static void Main(string[] args)
+		{
+			Application.Init();
+			Monitor m = new Monitor();
+			MainWindow win = new MainWindow(m);
+			win.Show();
+			Application.Run();
+		}
+	}
+}
