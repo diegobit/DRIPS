cc91d97e929ad051c2d28c3c1f8ef5fd4049e259
Diego Giorgini <diego.giorgini@icloud.com>
2017-03-22 08:22:25 +0100

    First attempt to read data from the serial port.

--- monitor/monitor/Program.cs
+++ monitor/monitor/Program.cs
@@ -6,18 +6,50 @@ namespace monitor
 {
 	public class Monitor
 	{
-		public void SaySomething()
-		{
-			Console.Write("Something... ");
-		}
-
 		public static void Main(string[] args)
 		{
 			Application.Init();
 			Monitor m = new Monitor();
-			MainWindow win = new MainWindow(m);
-			win.Show();
+			MainWindow w = new MainWindow(m);
+			Serial s = new Serial(w);
+			w.ShowAll();
+
 			Application.Run();
+		}
+	}
+
+	class Serial
+	{
+		MainWindow w;
+		// Create the serial port with basic settings
+		SerialPort port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+
+		public Serial(MainWindow window)
+		{
+			w = window;
+
+			// Attach a method to be called when there is data waiting in the port's buffer
+			port.DataReceived += new SerialDataReceivedEventHandler(dataReceivedHandler);
+
+			// Begin communications
+			//port.Open(); //FIXME: exception
+
+			// Enter an application loop to keep this thread alive
+			Application.Run(); //TODO ????
+		}
+
+		void dataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+		{
+			// Show all the incoming data in the port's buffer
+			string data = port.ReadExisting();
+
+			Console.WriteLine(data);
+
+			// Ensure interface updates are executed on main loop
+			Gtk.Application.Invoke(delegate
+			{
+				w.UpdateActionText(data);
+			});
 		}
 	}
 }
