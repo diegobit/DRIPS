3c03f7c17a0f890a58f15fb36b9d30eb2f9af865
Diego Giorgini <diego.giorgini@icloud.com>
2017-03-23 22:58:02 +0100

    Refactor Monitor: separated Serial class

--- monitor/monitor/Monitor.cs
+++ monitor/monitor/Monitor.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Gtk;
-using System.IO.Ports;
 
 namespace monitor
 {
@@ -12,39 +11,11 @@ namespace monitor
 			Monitor m = new Monitor();
 			MainWindow w = new MainWindow(m);
 
-			Serial s = new Serial(w);
+			Serial s = new Serial(w, "/dev/tty.usbmodem1D121");
+			s.startListening();
 			w.ShowAll();
 
 			Application.Run();
-		}
-	}
-
-	class Serial
-	{
-		MainWindow w;
-		//SerialPort port = new SerialPort("/dev/tty.usbmodem1D121", 9600, Parity.None, 19, StopBits.One);
-		SerialPort sp;
-
-		public Serial(MainWindow window)
-		{
-			w = window;
-
-			// Create the serial port with basic settings
-			sp = new SerialPort("/dev/tty.usbmodem1D111", 9600);
-			//sp.ReadTimeout = 400;
-
-			sp.Open();
-
-			Console.Write(sp.ReadLine()); // removes the \n
-			Console.Write(sp.ReadLine());
-			Console.Write(sp.ReadLine());
-			Console.Write(sp.ReadLine());
-
-			//// Ensure interface updates are executed on main loop
-			//Gtk.Application.Invoke(delegate
-			//{
-			//	w.UpdateActionText(data);
-			//});
 		}
 	}
 }
--- monitor/monitor/Serial.cs
+++ monitor/monitor/Serial.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
 namespace monitor
 {
 	public class Serial
 	{
-		public Serial()
+		MainWindow w;
+		//SerialPort port = new SerialPort("/dev/tty.usbmodem1D121", 9600, Parity.None, 19, StopBits.One);
+		string port = "";
+		SerialPort sp;
+
+		public Serial(MainWindow window, string port)
 		{
+			w = window;
+			this.port = port;
+
+			// Create the serial port with basic settings
+			sp = new SerialPort(port, 345600); //TODO: better port choice
+																 //sp.ReadTimeout = 400;
+			sp.Open();
+
+			//// Ensure interface updates are executed on main loop
+			//Gtk.Application.Invoke(delegate
+			//{
+			//	w.UpdateActionText(data);
+			//});
+		}
+
+		public void startListening()
+		{
+			Thread t = new Thread(delegate()
+			{
+				while (true)
+				{
+					string a = sp.ReadLine();
+					Console.Write(a.Length + "-" + a + "\n");
+				}
+			});
+			t.Start();
 		}
 	}
 }
