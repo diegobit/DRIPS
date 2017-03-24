9c356737ba93bcfa432c412f9eb77ba21653cbcb
Daniele Di Sarli <danieleds0@gmail.com>
2017-03-24 23:25:02 +0100

    Update spectrum viewer to new message format

--- spectrum_viewer/main.py
+++ spectrum_viewer/main.py
@@ -9,34 +9,24 @@ ser = serial.Serial('/dev/ttyACM3', 9600)
 def getFFTData():
     print("Syncing...")
     line = ""
-    while line != b'--begin-fft--\r\n':
+    while line[0] != b'R':
         line = ser.readline()
     
-    print("Read start")
-    xvals = []
-    yvals = []
-    while line != b'--end-fft--\r\n':
-        line = ser.readline()
-        if line != b'--end-fft--\r\n':
-            strdata = line.decode("utf-8").strip()
-            x1x2, y = strdata.split(" = ")
-            x1, x2 = x1x2.split(" -> ")
-            x = (float(x1) + float(x2)) / 2.0
-            
-            xvals.append(x)
-            yvals.append(float(y))
-    print("Read end")
+    yvals = line.decode("utf-8").strip()[1:].split(',')
+    yvals = [float(v) for v in yvals]
     
     # Remove low frequencies
     yvals[0] = 0
     yvals[1] = 0
     yvals[2] = 0
+
+    xvals = [i for i in range(len(yvals))]
     return xvals, yvals
     
 
 
 def animate(i):
-    x, y = getFFTData();
+    x, y = getFFTData()
     ax.cla()
     ax.bar(x, y)
 
