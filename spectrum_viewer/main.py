3cf92daf9f633cf72dfe961db53ea7b0c918ec96
Daniele Di Sarli <danieleds0@gmail.com>
2017-03-27 14:35:19 +0200

    Aggiornamento protocollo e vista di tutti e tre gli spettri

--- spectrum_viewer/main.py
+++ spectrum_viewer/main.py
@@ -1,38 +1,70 @@
 import serial
+import threading
+import queue
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-ser = serial.Serial('/dev/ttyACM3', 9600)
-
-def getFFTData():
-    print("Syncing...")
-    line = ""
-    while line[0] != b'R':
-        line = ser.readline()
-    
-    yvals = line.decode("utf-8").strip()[1:].split(',')
-    yvals = [float(v) for v in yvals]
-    
-    # Remove low frequencies
-    yvals[0] = 0
-    yvals[1] = 0
-    yvals[2] = 0
-
-    xvals = [i for i in range(len(yvals))]
-    return xvals, yvals
-    
+plotdataL = queue.Queue()
+plotdataF = queue.Queue()
+plotdataR = queue.Queue()
 
 
-def animate(i):
-    x, y = getFFTData()
-    ax.cla()
-    ax.bar(x, y)
+def retrievePlotData():
+    ser = serial.Serial('/dev/ttyACM0', 230400)
+    ser.readline() # Discard the first (possibly partial) line
+    while True:
+        print("Syncing...")
+        line = b''
+        while len(line) == 0 or chr(line[0]) not in ['L', 'F', 'R']:
+            line = ser.readline()
 
-fig, ax = plt.subplots()
+        yvals = line.decode('utf-8').strip()[2:].split(',')
+        yvals = [float(v) for v in yvals]
 
-rects = ax.bar([], [])
+        # Remove low frequencies
+        yvals[0] = 0
+        yvals[1] = 0
+        yvals[2] = 0
 
-ani = animation.FuncAnimation(fig, animate)
-plt.show()
+        xvals = [i for i in range(len(yvals))]
+        if chr(line[0]) == 'L':
+            plotdataL.put((xvals, yvals))
+        elif chr(line[0]) == 'F':
+            plotdataF.put((xvals, yvals))
+        elif chr(line[0]) == 'R':
+            plotdataR.put((xvals, yvals))
+
+def main():
+    def animate(i):
+        try:
+            x, y = plotdataL.get(block=False, timeout=None)
+            plotL.cla()
+            plotL.bar(x, y)
+        except queue.Empty:
+            pass
+
+        try:
+            x, y = plotdataF.get(block=False, timeout=None)
+            plotF.cla()
+            plotF.bar(x, y)
+        except queue.Empty:
+            pass
+
+        try:
+            x, y = plotdataR.get(block=False, timeout=None)
+            plotR.cla()
+            plotR.bar(x, y)
+        except queue.Empty:
+            pass
+
+    fig = plt.figure()
+    plotL = fig.add_subplot(131)
+    plotF = fig.add_subplot(132)
+    plotR = fig.add_subplot(133)
+    ani = animation.FuncAnimation(fig, animate, interval=100)
+    plt.show()
+
+threading.Thread(target=retrievePlotData).start()
+threading.Thread(target=main).start()
