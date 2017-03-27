de77c85a65f09b6be0efa2ad5890a8add53183cc
Daniele Di Sarli <danieleds0@gmail.com>
2017-03-28 01:20:08 +0200

    Don't completely redraw plots

--- spectrum_viewer/main.py
+++ spectrum_viewer/main.py
@@ -60,47 +60,58 @@ def main():
     def animate(i):
         try:
             x, y = plotdataL.get(block=False, timeout=None)
-            plotL.cla()
-            plotL.plot(x, y, 'C0')
+            plotL.lines[0].set_data(x, y)
         except queue.Empty:
             pass
 
         try:
             x, y = plotdataF.get(block=False, timeout=None)
-            plotF.cla()
-            plotF.plot(x, y, 'C1')
+            plotF.lines[0].set_data(x, y)
         except queue.Empty:
             pass
 
         try:
             x, y = plotdataR.get(block=False, timeout=None)
-            plotR.cla()
-            plotR.plot(x, y, 'C2')
+            plotR.lines[0].set_data(x, y)
         except queue.Empty:
             pass
         
-        plotLFR.cla()
-        plotLFR.plot(plotL.lines[0].get_xdata(), plotL.lines[0].get_ydata(), 'C0',
-                     plotF.lines[0].get_xdata(), plotF.lines[0].get_ydata(), 'C1',
-                     plotR.lines[0].get_xdata(), plotR.lines[0].get_ydata(), 'C2')
-        
-        plotL.set_title("Left sensor")
-        plotF.set_title("Front sensor")
-        plotR.set_title("Right sensor")
-        plotLFR.set_title("All sensors")
+        plotLFR.lines[0].set_data(plotL.lines[0].get_xdata(), plotL.lines[0].get_ydata())
+        plotLFR.lines[1].set_data(plotF.lines[0].get_xdata(), plotF.lines[0].get_ydata())
+        plotLFR.lines[2].set_data(plotR.lines[0].get_xdata(), plotR.lines[0].get_ydata())
         
         for plot in [plotL, plotF, plotR, plotLFR]:
-            plot.set_xlabel("Frequency (Hz)")
-            plot.set_ylabel("Intensity")
+            if len(plot.lines) > 0 and len(plot.lines[0].get_xdata()) > 0:
+                plot.set_xlim([0, plot.lines[0].get_xdata()[-1]])
             plot.set_ylim([0, 1024])
+        
+        return plotL.lines[0], plotF.lines[0], plotR.lines[0], plotLFR.lines[0], plotLFR.lines[1], plotLFR.lines[2]
 
     fig = plt.figure()
+    
     plotL = plt.subplot2grid((5, 3), (3, 0), rowspan=2)
     plotF = plt.subplot2grid((5, 3), (3, 1), rowspan=2)
     plotR = plt.subplot2grid((5, 3), (3, 2), rowspan=2)
     plotLFR = plt.subplot2grid((5, 3), (0, 0), colspan=3, rowspan=3)
+    
+    plotL.plot([0], [0], 'C0')
+    plotF.plot([0], [0], 'C1')
+    plotR.plot([0], [0], 'C2')
+    plotLFR.plot([0, 1], [0, 1], 'C0', [0, 1], [0, 1], 'C1', [0, 1], [0, 1], 'C2')
+    
+    plotL.set_title("Left sensor")
+    plotF.set_title("Front sensor")
+    plotR.set_title("Right sensor")
+    plotLFR.set_title("All sensors")
+
+    for plot in [plotL, plotF, plotR, plotLFR]:
+        plot.set_xlabel("Frequency (Hz)")
+        plot.set_ylabel("Intensity")
+        plot.set_ylim([0, 1024])
+    
     plt.tight_layout()
     fig.subplots_adjust(hspace=1.5)
+    
     ani = animation.FuncAnimation(fig, animate, interval=100)
     plt.show()
 
