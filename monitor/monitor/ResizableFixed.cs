46dd2361d20eed8eaf5727331ed3c21f00c707fd
Diego Giorgini <diego.giorgini@icloud.com>
2017-04-01 22:05:25 +0200

    Add ResizableFixed class. NOT used

---
+++ monitor/monitor/ResizableFixed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace monitor
+{
+	//[System.ComponentModel.ToolboxItem(true)]
+	public class ResizableFixed : Gtk.Fixed
+	{
+		public ResizableFixed()
+		{
+			//this.Build();
+
+		}
+
+		protected override void OnSizeAllocated(Gdk.Rectangle allocation)
+		{
+			Console.WriteLine("OnSizeAllocated Resizable + w:" + allocation.Width + " h:" + allocation.Height);
+			Foreach((widget) => Console.Write(widget.GetType()));
+			//foreach (Widget c in Children)
+			//{
+			//	Console.Write(c.GetType());
+			//}
+			//base.OnSizeAllocated(allocation);
+		}
+	}
+}
