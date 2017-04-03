c21f8ce07fc3f120344f472953142c2ab1f20811
Diego Giorgini <diego.giorgini@icloud.com>
2017-04-03 21:33:54 +0200

    Add Road class

---
+++ monitor/monitor/Road.cs
@@ -0,0 +1,62 @@
+﻿namespace monitor
+{
+	public class Road
+	{
+		public RoadID Id { get; private set; }
+
+		public string Manufacturer { get; set; }
+		public string Model { get; set; }
+		public int Orientation { get; set; } = -1;
+		public Priority Priority { get; set; }
+		public Action RequestedAction { get; set; }
+		public Action CurrentAction { get; set; }
+
+
+
+		public Road(RoadID id)
+		{
+			Id = id;
+		}
+
+
+
+		/*
+		 * Returns true if the road id is empty, false if there's a car
+		 */
+		bool IsEmpty()
+		{
+			return Manufacturer == null & Model == null && Orientation == -1 &&
+				Priority == 0 && CurrentAction == 0 && RequestedAction == 0;
+		}
+
+		/*
+		 * Returns true if there's a car on the road id, but I only know it's orientation
+		 */
+		bool IsPartial()
+		{
+			return Manufacturer == null && Model == null && Orientation != -1 &&
+				Priority == 0 && CurrentAction == 0 && RequestedAction == 0;
+		}
+
+		/*
+		 * Returns true if there's a car on the road id, and I have all the informations about it
+		 */
+		bool IsComplete()
+		{
+			return Manufacturer != null && Model != null && Orientation == -1 &&
+				Priority != 0 && CurrentAction != 0 && RequestedAction != 0;
+		}
+
+
+
+		public void RemoveCar()
+		{
+			Manufacturer = null;
+			Model = null;
+			Orientation = -1;
+			Priority = 0;
+			CurrentAction = 0;
+			RequestedAction = 0;
+		}
+	}
+}
