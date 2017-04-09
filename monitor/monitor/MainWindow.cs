2d633504c7732619c4c83bf0ff8dc32c0a000a5b
Diego Giorgini <diego.giorgini@icloud.com>
2017-04-09 21:14:57 +0200

    Add light signals logic. Refactor things...

--- monitor/monitor/MainWindow.cs
+++ monitor/monitor/MainWindow.cs
@@ -16,7 +16,11 @@ public partial class MainWindow : Window
 
 	Layout container;
 	Image crossroadImage;
-    Dictionary<RoadID, Tuple<Image, Label>> roads;
+    /**
+     * For each road I keep 4 things:
+     * car image, label, left signal, right signal
+     */
+    Dictionary<RoadID, Tuple<Image, Label, Image, Image>> roads;
 
 
 
@@ -25,37 +29,15 @@ public partial class MainWindow : Window
 		Build();
         Resize(1000, 700);
 
-        unknownImagePath = "monitor.resources.car" + resDiv + "Unknown" + resDiv + "Unknown" + imageExtension;
+		unknownImagePath = "monitor.resources.car" + resDiv + "Unknown" + resDiv + "Unknown" + imageExtension;
 
-        roads = new Dictionary<RoadID, Tuple<Image, Label>>();
+        roads = new Dictionary<RoadID, Tuple<Image, Label, Image, Image>>();
+
         container = new Layout(null, null);
+		Add(container);
 
 		crossroadImage = Image.LoadFromResource("monitor.resources.crossroad.png");
-        container.Put(crossroadImage, 0, 0);
-
-		// Create a textview for each car in the road
-		var roadvalues = (RoadID[])Enum.GetValues(typeof(RoadID));
-        foreach (RoadID road in roadvalues)
-		{
-            if (road != RoadID.None)
-            {
-                Label l = new Label();
-                l.SetSizeRequest(280, 120);
-                l.ModifyBase(StateType.Normal, new Gdk.Color(230, 230, 230));
-                l.ModifyFont(Pango.FontDescription.FromString("Arial 20"));
-                l.Text = MakeCarLabelText();
-                if (road == RoadID.Left || road == RoadID.Top)
-                    l.Justify = Justification.Right;
-
-                roads.Add(road, Tuple.Create((Image)null, l));
-
-                Tuple<int, int> pos = ComputeLabelPosition(road, l);
-                container.Put(l, pos.Item1, pos.Item2);
-            }
-		}
-
-		// Put the container in the window
-		Add(container);
+		container.Put(crossroadImage, 0, 0);
 
 		// Listen for window resizing events
 		SizeAllocated += delegate
@@ -66,13 +48,32 @@ public partial class MainWindow : Window
 				stopPropagate = true;
 			}
 		};
+
+		// Create a textview for each car in the road
+		var roadvalues = (RoadID[])Enum.GetValues(typeof(RoadID));
+		foreach (RoadID road in roadvalues)
+		{
+			if (road != RoadID.None)
+			{
+				Label label = LoadLabel(road);
+				Tuple<Image, Image> s = LoadSignalImages(road);
+
+				roads.Add(road, Tuple.Create((Image)null, label, s.Item1, s.Item2));
+
+				PlaceLabel(label, road);
+				Application.Invoke(delegate /* FIXME: workaround: by executing it this way 
+				the method is delayed long enough for the Hide() on the signals to be effective */
+				{
+					PlaceSignals(s.Item1, s.Item2, road);
+				});
+
+			}
+		}
 	}
-
-
 
 	public void UpdateRoad(Road road)
 	{
-        roads.TryGetValue(road.Id, out Tuple<Image, Label> car);
+        roads.TryGetValue(road.Id, out Tuple<Image, Label, Image, Image> car);
         string expectedImagePath = "monitor.resources.car" + resDiv + road.Manufacturer + resDiv + road.Model + imageExtension;
 
 		// Update text beside the car
@@ -90,7 +91,7 @@ public partial class MainWindow : Window
 			{
                 // The car has changed. Load the new image and place it;
 			    Image carImg = LoadCarImage(expectedImagePath, road.Id);
-                roads[road.Id] = Tuple.Create(carImg, car.Item2);
+                roads[road.Id] = Tuple.Create(carImg, car.Item2, car.Item3, car.Item4);
                 RemoveCar(prevPath);
 				PlaceCar(carImg, road);
 			}
@@ -99,9 +100,30 @@ public partial class MainWindow : Window
 		{
             // Image not present, I place it
 			Image carImg = LoadCarImage(expectedImagePath, road.Id);
-            roads[road.Id] = Tuple.Create(carImg, car.Item2);
+            roads[road.Id] = Tuple.Create(carImg, car.Item2, car.Item3, car.Item4);
 			PlaceCar(carImg, road);
  		}
+
+		// Update signals
+		Image leftSignal = car.Item3;
+		Image rightSignal = car.Item4;
+		leftSignal.Hide(); // Done to synchronize the GIF playback of the two signals
+		rightSignal.Hide();
+		if (road.Priority == Priority.High)
+		{
+			leftSignal.Show();
+			rightSignal.Show();
+		}
+		else if (road.RequestedAction == monitor.Action.Left)
+		{
+			leftSignal.Show();
+			rightSignal.Hide();
+		}
+		else if (road.RequestedAction == monitor.Action.Right)
+		{
+			leftSignal.Hide();
+			rightSignal.Show();
+		}
 	}
 
 
@@ -122,6 +144,44 @@ public partial class MainWindow : Window
 			car.Name = id + unknownImagePath;
 		}
 		return car;
+	}
+
+    Tuple<Image, Image> LoadSignalImages(RoadID road)
+    {
+        Image l = new Image();
+        Image r = new Image();
+		switch (road)
+		{
+			case (RoadID.Bottom):
+				l.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_left.gif");
+				r.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_right.gif");
+				break;
+			case (RoadID.Left):
+				l.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_up.gif");
+				r.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_down.gif");
+				break;
+			case (RoadID.Top):
+				l.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_right.gif");
+				r.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_left.gif");
+				break;
+			case (RoadID.Right):
+				l.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_down.gif");
+				r.PixbufAnimation = new Gdk.PixbufAnimation(null, "monitor.resources.signal_up.gif");
+				break;
+		}
+        return Tuple.Create(l, r);
+    }
+
+	Label LoadLabel(RoadID road)
+	{
+		Label label = new Label();
+		label.SetSizeRequest(280, 120);
+		label.ModifyBase(StateType.Normal, new Gdk.Color(230, 230, 230));
+		label.ModifyFont(Pango.FontDescription.FromString("Arial 20"));
+		label.Text = MakeCarLabelText();
+		if (road == RoadID.Left || road == RoadID.Top)
+			label.Justify = Justification.Right;
+		return label;
 	}
 
     void RemoveCar(string imageName)
@@ -146,13 +206,33 @@ public partial class MainWindow : Window
     void PlaceCar(Image car, Road road)
 	{
         Tuple<int, int> pos = ComputeCarPosition(road.Id, car);
+		car.Pixbuf = car.Pixbuf.RotateSimple((Gdk.PixbufRotation)road.Orientation); //TODO: only allowed 90 degrees step
 
 		Application.Invoke(delegate
 		{
-            car.Pixbuf = car.Pixbuf.RotateSimple((Gdk.PixbufRotation) road.Orientation); //TODO: only allowed 90 degrees step
 			container.Put(car, pos.Item1, pos.Item2);
-			container.ShowAll();
+			car.Show();
 		});
+	}
+
+	void PlaceSignals(Image leftSignal, Image rightSignal, RoadID road)
+    {
+		Tuple<int, int, int, int> pos = ComputeSignalPositions(road, leftSignal);
+		int xl = pos.Item1;
+		int yl = pos.Item2;
+		int xr = pos.Item3;
+		int yr = pos.Item4;
+
+		container.Put(leftSignal, xl, yl);
+		container.Put(rightSignal, xr, yr);
+		leftSignal.Hide();
+		rightSignal.Hide();
+    }
+
+	void PlaceLabel(Label label, RoadID road)
+	{
+		Tuple<int, int> pos = ComputeLabelPosition(road, label);
+		container.Put(label, pos.Item1, pos.Item2);
 	}
 
 
@@ -220,6 +300,52 @@ public partial class MainWindow : Window
 		return Tuple.Create(x, y);
 	}
 
+	/**
+	 * returns the positions of the signals of the car on `road`.
+	 * Assumptions: image `signal` already rotated; the two signals have the same size
+	 */
+	Tuple<int, int, int, int> ComputeSignalPositions(RoadID road, Image signal)
+    {
+		int crossW = crossroadImage.Allocation.Width;
+		int crossH = crossroadImage.Allocation.Height;
+		int sW = signal.PixbufAnimation.Width;
+		int sH = signal.PixbufAnimation.Height;
+
+		int xl = 0;
+		int yl = 0;
+		int xr = 0;
+		int yr = 0;
+		switch (road)
+		{
+			case (RoadID.Bottom):
+				xl = crossW / 2;
+				yl = crossH / 2 + rRadH - sH;
+				xr = xl + rRadW - sW;
+				yr = yl;
+				break;
+			case (RoadID.Left):
+				xl = crossW / 2 - rRadW;
+				yl = crossH / 2;
+				xr = xl;
+				yr = yl + rRadH - sH;
+				break;
+			case (RoadID.Top):
+				xl = crossW / 2 - sW;
+				xr = xl - rRadW + sW;
+				yl = crossH / 2 - rRadH;
+				yr = yl;
+				break;
+			case (RoadID.Right):
+				xl = crossW / 2 + rRadW - sW;
+				xr = xl;
+				yl = crossH / 2 - sH;
+				yr = yl - rRadH + sH;
+				break;
+		}
+
+		return Tuple.Create(xl, yl, xr, yr);
+    }
+
 	string MakeCarLabelText(Road road)
 	{
         if (road.IsEmpty())
@@ -256,9 +382,11 @@ public partial class MainWindow : Window
         {
         foreach (RoadID road in roads.Keys)
         {
-            Tuple<Image, Label> t = roads[road];
+            Tuple<Image, Label, Image, Image> t = roads[road];
             Image car = t.Item1;
             Label label = t.Item2;
+			Image leftSignal = t.Item3;
+			Image rightSignal = t.Item4;
 
 			if (car != null)
 			{
@@ -270,6 +398,13 @@ public partial class MainWindow : Window
 			{
 				Tuple<int, int> pos = ComputeLabelPosition(road, label);
 				container.Move(label, pos.Item1, pos.Item2);
+			}
+
+			if (leftSignal != null && rightSignal != null)
+			{
+				Tuple<int, int, int, int> pos = ComputeSignalPositions(road, leftSignal);
+				container.Move(leftSignal, pos.Item1, pos.Item2);
+				container.Move(rightSignal, pos.Item3, pos.Item4);
 			}
         }
     }
