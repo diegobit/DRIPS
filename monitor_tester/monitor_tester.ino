de5ebedbdaa2c2c4a2171acfa68bd15631fcf6e4
Diego Giorgini <diego.giorgini@icloud.com>
2017-05-05 23:20:18 +0200

    Update monitor_tester with the new infomessage format

--- monitor_tester/monitor_tester.ino
+++ monitor_tester/monitor_tester.ino
@@ -2,7 +2,7 @@
  *  This code sends some test packets through the serial port
  *  to test the communication and the monitor
  */
- 
+
  /************
  * TESTS
  ************
@@ -22,26 +22,16 @@ uint16_t bins3[FFT_N / 2];
 
 
 
-void sendPartialInfoMessage(char roadId, uint16_t orientation) {
-  Serial.print('I');
-  Serial.print(roadId);
-  Serial.print("                "); // 16 spaces
-  if (orientation < 10) Serial.print(F("  "));
-  else if (orientation < 100) Serial.write(' ');
-  Serial.print(orientation);
-  Serial.print("000");
-  Serial.print('\n');
-}
-
 /*
  * Sends an info-message to the serial port
  * Manufacturer and model should be already padded
  */
-void sendInfoMessage(char roadId, String manufacturer, String model,
+void sendInfoMessage(char roadId, char isEmpty, String manufacturer, String model,
                      uint16_t orientation, char priority, char requestedAction, char currentAction) {
 
   Serial.print('I');
   Serial.print(roadId);
+  Serial.print(isEmpty);
   Serial.print(manufacturer);
   Serial.print(model);
   if (orientation < 10) Serial.print(F("  "));
@@ -81,96 +71,107 @@ void sendFrequencyMessage(char type, uint16_t fft_bins[]) {
 
 
 
-void generalTest() {
-  delay(2000); // 2 seconds
+// void generalTest() {
+//   delay(2000); // 2 seconds
 
-  /**********
-   * The car just arrived, it thinks it is alone (its sensors see nothing)
-   **********/
-  sendInfoMessage('M', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
+//   /**********
+//    * The car just arrived, it thinks it is alone (its sensors see nothing)
+//    **********/
+//   sendInfoMessage('M', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
 
-  for (uint16_t i = 0; i < FFT_N / 2; i++) {
-    bins1[i] = i + random(21);
-    bins2[i] = i + random(21);
-    bins3[i] = i + random(21);
-  }
+//   for (uint16_t i = 0; i < FFT_N / 2; i++) {
+//     bins1[i] = i + random(21);
+//     bins2[i] = i + random(21);
+//     bins3[i] = i + random(21);
+//   }
 
-  delay(2000); // 2 seconds
+//   delay(2000); // 2 seconds
 
-  /**********
-   * The car sees 3 other cars, it has not joined the network
-   **********/
-  sendInfoMessage('M', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
-  sendPartialInfoMessage('L', 270);
-  sendPartialInfoMessage('F', 180);
-  sendPartialInfoMessage('R', 90);
+//   /**********
+//    * The car sees 3 other cars, it has not joined the network
+//    **********/
+//   sendInfoMessage('M', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
+//   sendPartialInfoMessage('L', 270);
+//   sendPartialInfoMessage('F', 180);
+//   sendPartialInfoMessage('R', 90);
 
-  bins1[9] = 700 + random(21);
-  bins1[10] = 700 + random(21);
-  bins1[11] = 700 + random(21);
-  bins2[20] = 400 + random(21);
-  bins2[21] = 400 + random(21);
-  bins2[22] = 400 + random(21);
-  bins3[30] = 900 + random(21);
-  bins3[31] = 900 + random(21);
-  bins3[32] = 900 + random(21);
-  sendFrequencyMessage('L', bins1);
-  sendFrequencyMessage('F', bins2);
-  sendFrequencyMessage('R', bins3);
+//   bins1[9] = 700 + random(21);
+//   bins1[10] = 700 + random(21);
+//   bins1[11] = 700 + random(21);
+//   bins2[20] = 400 + random(21);
+//   bins2[21] = 400 + random(21);
+//   bins2[22] = 400 + random(21);
+//   bins3[30] = 900 + random(21);
+//   bins3[31] = 900 + random(21);
+//   bins3[32] = 900 + random(21);
+//   sendFrequencyMessage('L', bins1);
+//   sendFrequencyMessage('F', bins2);
+//   sendFrequencyMessage('R', bins3);
 
-  delay(2000); // 2 seconds
+//   delay(2000); // 2 seconds
 
-  /**********
-   * The car sees 2 other cars
-   **********/
-  sendInfoMessage('M', "Vlkswagn", "Beetle  ",   0, 'N', 'L', 'S');
-  sendInfoMessage('L', "Police  ", "Police  ", 270, 'N', 'A', 'S');
-  sendInfoMessage('R', "Tesla   ", "Model S ",  90, 'N', 'L', 'L');
+//   /**********
+//    * The car sees 2 other cars
+//    **********/
+//   sendInfoMessage('M', "Vlkswagn", "Beetle  ",   0, 'N', 'L', 'S');
+//   sendInfoMessage('L', "Police  ", "Police  ", 270, 'N', 'A', 'S');
+//   sendInfoMessage('R', "Tesla   ", "Model S ",  90, 'N', 'L', 'L');
 
-  delay(2000);
+//   delay(2000);
 
-  /**********
-   * The car sees 2 other cars. The police car wants priority
-   **********/
-  sendInfoMessage('M', "Vlkswagn", "Beetle  ",  0, 'N', 'L', 'S');
-  sendInfoMessage('L', "Police  ", "Police  ", 270, 'Y', 'A', 'S');
-  sendInfoMessage('R', "Tesla   ", "Model S ", 180, 'N', 'L', 'L');
+//   /**********
+//    * The car sees 2 other cars. The police car wants priority
+//    **********/
+//   sendInfoMessage('M', "Vlkswagn", "Beetle  ",  0, 'N', 'L', 'S');
+//   sendInfoMessage('L', "Police  ", "Police  ", 270, 'Y', 'A', 'S');
+//   sendInfoMessage('R', "Tesla   ", "Model S ", 180, 'N', 'L', 'L');
 
-  delay (2000);
-}
+//   delay (2000);
+// }
 
 
 
 
 
 void infoMessageTest() {
+  // my road full
   delay(3000);
 
-  sendInfoMessage('M', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
+  sendInfoMessage('M', '0', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
 
-  delay(3000);
+  delay(500);
 
-  sendInfoMessage('M', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
-
-  delay(3000);
-
-  sendPartialInfoMessage('L', 270);
-
-  delay(3000);
-
-  sendInfoMessage('R', "Tesla   ", "Model S ", 90, 'N', 'L', 'L');
+  // My road full (duplicate)
+  sendInfoMessage('M', '0', "Vlkswagn", "Beetle  ", 0, 'N', 'L', 'S');
 
   delay(3000);
 
-  sendInfoMessage('L', "Police  ", "Police  ",  270, 'N', 'A', 'S');
+  // Left   empty
+  // Front  unknown
+  // Right  full
+  sendInfoMessage('L', '1', "        ", "        ", 0, 'N', 'L', 'S');
+  sendInfoMessage('A', '0', "        ", "        ", 180, 'N', '0', '0');
+  sendInfoMessage('R', '0', "Tesla   ", "Model S ", 90, 'N', 'L', 'S');
+
+  delay(3000);
+
+  // Left   full
+  // Front  full
+  // Right  empty
+  sendInfoMessage('L', '0', "Police  ", "Police  ", 270, 'N', 'L', 'S');
+  sendInfoMessage('A', '0', "Tesla   ", "Model S ", 180, 'N', 'L', 'S');
+  sendInfoMessage('R', '1', "        ", "        ", 90, 'N', 'L', 'S');
+
+  delay(3000);
+
+  // Left   full  priority
+  // Front  full
+  // Right  full
+  sendInfoMessage('L', '0', "Police  ", "Police  ", 270, 'Y', 'L', 'L');
+  sendInfoMessage('A', '0', "Tesla   ", "Model S ", 180, 'N', 'L', 'S');
+  sendInfoMessage('R', '0', "Tesla   ", "Model S ", 90, 'N', 'L', 'S');
 
   delay (3000);
-
-  sendInfoMessage('L', "Police  ", "Police  ",  270, 'Y', 'A', 'S');
-
-  delay (3000);
-
-  sendInfoMessage('T', "Tesla   ", "Model S ",  0, 'N', 'S', 'S');
 }
 
 
